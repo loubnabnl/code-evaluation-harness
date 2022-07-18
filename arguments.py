07d560bdde8f432ecc2d9df8ed775fca72cb7c1e
loubnabnl <loubnabenallal1999@gmail.com>
2022-07-18 16:41:30 +0000

    fix arguments

--- arguments.py
+++ arguments.py
@@ -24,7 +24,7 @@ class EvalArguments:
         default=None,
         metadata={"help": "The number of APPS tasks to run. If not specified all tasks are evaluated."},
     )
-    level_apps: Optional[int] = field(
+    level_apps: Optional[str] = field(
         default="all",
         metadata={"help": "The difficulty level to use for APPS, among introductory, interview, competition and all."},
     )
@@ -36,7 +36,6 @@ class EvalArguments:
     max_new_tokens_apps: Optional[int] = field(default=124, metadata={"help": "Maximum number of newly generated tokens."})
     top_k: Optional[int] = field(default=0, metadata={"help": "Top-k parameter used for generation."})
     top_p: Optional[float] = field(default=0.95, metadata={"help": "Top-p parameter used for nucleus sampling."})
-    batch_size: Optional[int] = field(default=1, metadata={"help": "Number of generations to run in parallel."})
     n_samples: Optional[int] = field(
         default=200, metadata={"help": "Number of completions to generate for each sample."}
     )
